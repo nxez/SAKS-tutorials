3ef669d8826e5211f2089a284c7747884aa3b760
spoony <spoonysonny@163.com>
2015-11-22 21:47:24 +0800

    modify

--- chime/chime.py
+++ chime/chime.py
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# tutorials url: http://shumeipai.nxez.com/2015/10/07/saks-diy-tutorials-chime.html
+# tutorials url: http://shumeipai.nxez.com/2015/11/22/saks-diy-tutorials-chime.html
 
 __author__ = 'Spoony'
 __license__  = 'Copyright (c) 2015 NXEZ.COM'
