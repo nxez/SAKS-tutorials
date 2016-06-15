0b90c76691a6700dc1c6bc9d7332cda89cb94be3
spoony <spoonysonny@163.com>
2016-06-15 22:36:39 +0800

    modify

--- chengying/main.py
+++ chengying/main.py
@@ -24,18 +24,20 @@ from sakshat import SAKSHAT
 import time
 import commands
 import sys, urllib, urllib2, json
+import requests
+import re
 
 #Declare the SAKS Board
 SAKS = SAKSHAT()
 
 __current_mode = 0
 __current_command = 0
-#__mode = [ '0', '1' ]
-__command = { '0': [ '.0', '.1' ], '1': [ '.0', '.1' ] }
+__modes = [ 'display', '1' ]
+__commands = { 'display': [ '.time', '.cputemp', '.roomtemp', '.citytemp', '.pm25', '.stockmarketindex' ], '1': [ '.0', '.1' ] }
 
 def display_cpu_temp():
     while True:
-        if __current_mode + __current_command == '':
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.cputemp':
             t = get_cpu_temp()
             # Uncomment if need print the temperature to screen
             #print("%.2f" % t)
@@ -49,7 +51,7 @@ def display_cpu_temp():
 
 def display_pm25():
     while True:
-        if __current_mode + __current_command == '':
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.pm25':
             pm25 = get_pm25()
             if pm25 == -1:
                 time.sleep(30)
@@ -91,7 +93,7 @@ def display_pm25():
 
 def display_city_temp():
     while True:
-        if __current_mode + __current_command == '':
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.citytemp':
             temp = get_city_temp()
             #返回值为 -128.0 表示读取失败
             if temp == -128.0 :
@@ -108,7 +110,7 @@ def display_city_temp():
 
 def display_room_temp():
     while True:
-        if __current_mode + __current_command == '':
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.roomtemp':
             #从 ds18b20 读取温度（摄氏度为单位）
             temp = SAKS.ds18b20.temperature
             #返回值为 -128.0 表示读取失败
@@ -132,7 +134,7 @@ def display_time():
     __alarm_time = "18:10:00"
 
     while True:
-        if __current_mode + __current_command == '':
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.time':
             # 以下代码获取系统时间、时、分、秒、星期的数值
             t = time.localtime()
             h = t.tm_hour
@@ -170,8 +172,19 @@ def display_time():
             break
 
 def display_stock_market_index():
-    if __current_mode + __current_command == '':
-        pass
+    while True:
+        if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.stockmarketindex':
+            pattern = re.compile(r'(\d*\.\d*)')
+            r = requests.get('http://hq.sinajs.cn/list=s_sh000001')
+            #print r.text.encode('utf-8')
+            #print r.encoding
+            match = pattern.findall(r.text)
+            if match:
+                return match[0]
+            else:
+                return 0
+        else:
+            break
     pass
 
 def alarm_process():
@@ -199,6 +212,29 @@ def tact_event_handler(pin, status):
     '''
     print('tact_event_handler')
     print("%d - %s" % (pin, status))
+    global  __current_command
+    if __current_command >= len(__commands['display']):
+        __current_command = 0
+    else:
+        __current_command += 1
+
+    print(__modes[__current_mode] + __commands[__modes[__current_mode]][__current_command])
+
+    if __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.cputemp':
+        display_cpu_temp()
+    elif __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.pm25':
+        display_pm25()
+    elif __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.citytemp':
+        display_city_temp()
+    elif __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.roomtemp':
+        display_room_temp()
+    elif __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.time':
+        display_time()
+    elif __modes[__current_mode] + __commands[__modes[__current_mode]][__current_command] == 'display.stockmarketindex':
+        display_stock_market_index()
+    else:
+        pass
+
 
 #def handler(signum, frame):
 #    SAKS.digital_display.off()
